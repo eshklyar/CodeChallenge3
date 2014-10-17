9e14b64d7c6452f1e767ba14375621b9078e5a5f
eshklyar <eshklyar@gmail.com>
2014-10-17 10:45:29 -0500

    this sucks

---
+++ CodeChallenge3/DivvyStation.h
@@ -0,0 +1,13 @@
+//
+//  DivvyStation.h
+//  CodeChallenge3
+//
+//  Created by Edik Shklyar on 10/17/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DivvyStation : NSObject
+
+@end
